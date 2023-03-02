55cbfe9d3fa5b2560d8110bad04959d0042c4b8a
balyar <denisbolurov@gmail.com>
2023-03-02 22:10:10 +0300

    added comments

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.base import View
 from .models import Post
+from .form import CommentsForm
 
 
 class PostView(View):
@@ -9,3 +10,20 @@ class PostView(View):
         posts = Post.objects.all()
         return render(request, 'blog/blog.html', {'post_list': posts})
 
+
+class PostDetail(View):
+    """Отдельная страница записи"""
+    def get(self, request, pk):
+        post = Post.objects.get(id=pk)
+        return render(request, 'blog/blog_detail.html', {'post': post})
+
+
+class AddComments(View):
+    """Добавление комментариев"""
+    def post(self, request, pk):
+        form = CommentsForm(request.POST)
+        if form.is_valid():
+            form = form.save(commit=False)
+            form.post_id = pk
+            form.save()
+        return redirect(f'/{pk}')
